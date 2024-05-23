819bfabc3237d1926cb6a04091af30b4968418cf
astroshaik <ahs2182@email.vccs.edu>
2024-05-22 22:41:02 -0400

    Html page setup

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from .views import index
+
 
 urlpatterns = [
-    path('', views.index, name='Login'),
-    path('Login', views.index, name='Login'),
+    path('', index),
+    path('Login', views.login, name='Login'),
+    path('Registration', views.registration, name='Registration'),
+    path('Homepage', views.homepage, name='Roomie'),
+    path('RoomieVal', views.roomieVal, name='RoomieVal'),
+    path('Calendar', views.calendar, name='Calendar'),
 ]
