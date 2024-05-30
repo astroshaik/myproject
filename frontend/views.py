c7d94855381911b9633b2adde61ea282e71b435c
astroshaik <ahs2182@email.vccs.edu>
2024-05-30 12:30:40 -0400

    delete old code

--- frontend/views.py
+++ frontend/views.py
@@ -17,67 +17,8 @@ import jwt
 from .forms import LoginForm, AllergyForm, RuleForm
 from api.models import Roomie, Task, Rule, Allergy
 
-def homepage(request):
-    roomies = Roomie.objects.all()
-    tasks = Task.objects.all()
-    rules = Rule.objects.all()
-    allergies = Allergy.objects.all()
-    roomie_data = []
 
-    # Extract tasks, rules, and allergies frome each roomie
-    for roomie in roomies:
-        
-        roomie_tasks = tasks.filter(roomie=roomie)
-        roomie_rules = rules.filter(agreement_roomie_ids__contains=[roomie.roomie_id])
-        roomie_allergies = allergies.filter(roomie_ids__contains=[roomie.roomie_id])
-    
-        roomie_data.append(
-            {'roomie': roomie, 'tasks': roomie_tasks, 'rules': roomie_rules, 'allergies': roomie_allergies}
-        )
-        
-    # if request.method == 'POST':
-    #     if 'add_allergy' in request.POST:
-    #         allergy_form = AllergyForm(request.POST)
-    #         if allergy_form.is_valid():
-    #             allergy_form.save()
-    #             return redirect('frontend/Homepage.html')
-    #     elif 'add_rule' in request.POST:
-    #         rule_form = RuleForm(request.POST)
-    #         if rule_form.is_valid():
-    #             rule_form.save()
-    #             return redirect('frontend/Homepage.html')
-    #     else:
-    #         allergy_form = AllergyForm()
-    #         rule_form = RuleForm()
 
-    data = {
-        'roomie_data': roomie_data,
-        'allergy_form': AllergyForm,
-        'rule_form': RuleForm,
-    }
-
-    return render(request, "frontend/Homepage.html", data)
-
-def allergy(request):
-    if request.method == 'POST':
-        form = AllergyForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('Roomie')
-        else:
-            form = AllergyForm()
-        return render(request, 'frontend/AddAllergy.html', {'form': form})
-
-def rule(request):
-    if request.method == 'POST':
-        form = RuleForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('Roomie')
-        else:
-            form = RuleForm()
-        return render(request, 'frontend/AddRule.html', {'form': form})
-    
 
 def get_tokens_for_user(user):
     refresh = RefreshToken.for_user(user)
